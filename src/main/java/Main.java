5b8dcc2df1e80f180bf34022112caf87ad83759d
Damian <damian.andrzej.muca@gmail.com>
2018-05-06 22:42:31 +0200

    Adnotacja dziedziczona przykład

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.lang.annotation.Annotation;
 
 public class Main {
+    public static void main(String[] args) {
+        AdnotacjaDziedziczona adnotacjaDziedziczona = B.class.getAnnotation(AdnotacjaDziedziczona.class);
+        System.out.println(adnotacjaDziedziczona.value());
+    }
 }
+
+@AdnotacjaDziedziczona("JAKAŚ WARTOŚĆ")
+class A{}
+
+class B extends A{}
